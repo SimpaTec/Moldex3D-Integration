eea7db3d90bfe1eec4c7a69d32f41d92224814b8
beingeek <furqan.790@outlook.com>
2023-07-28 17:15:52 +0500

    fix: restrict file dialog option to mac, file upload and process refactor

--- moldex3d_integration/moldex3d_integration/doctype/mac_file/mac_file_list.js
+++ moldex3d_integration/moldex3d_integration/doctype/mac_file/mac_file_list.js
@@ -1,7 +1,7 @@
 
 frappe.listview_settings['MAC File'] = {
     onload: function(list_view) {
-		//let me = this;
+		let me = this;
 		//var filedata = "";
 		list_view.page.add_inner_button(__("Process Mac File"), function() {
 					
@@ -12,13 +12,13 @@ frappe.listview_settings['MAC File'] = {
 							fieldname: "upload_component",
 							label: "Upload Component",
 							fieldtype: "HTML",
-							options: "<input id=\"upload_mac\" type=\"file\" value=\"process\">"
+							options: "<input id=\"upload_mac\" type=\"file\" accept=\".mac\" value=\"process\">"
 						},
 					],
 					primary_action(data) {
-						let filedata = $('#upload_mac')[0].files[0];						
-						if (filedata == undefined && filedata.name == null) {							
-						frappe.msgprint(__("Upload a .mac file"));
+						let filedata = $('#upload_mac')[0].files[0];
+						if (!filedata || (filedata && !filedata.name && filedata.type != 'mac')) {
+							frappe.throw(__("Upload a .mac file"));
 						} 
 						else {							
 
@@ -29,52 +29,12 @@ frappe.listview_settings['MAC File'] = {
 										data: list_view.doctype
 									},
 									callback: function (r) {										
-										if (r.message != null ) {
+										if (r.message) {
+											me.upload_and_attach_macfile(r, list_view.doctype, filedata)
 											frappe.show_alert({
-											message: __("Mac file Processed ... : ",r.message),
-											indicator: 'blue'
+												message: __("Mac file Processed ... : ",r.message),
+												indicator: 'blue'
 											});
-								
-										}
-										if(r.message && filedata){
-											let imagefile = new FormData();
-								
-											imagefile.append('doctype',list_view.doctype);
-											imagefile.append('docname', r.message);						
-								
-											imagefile.append('folder', "Home/"+list_view.doctype);						
-								
-											imagefile.append('file', filedata);
-
-											fetch('/api/method/upload_file', {
-												headers: {
-													'X-Frappe-CSRF-Token': frappe.csrf_token
-												},
-												method: 'POST',
-												body: imagefile
-											})
-											.then(res => res.json())
-											.then(data => {
-									
-												if (data.message){													
-											
-													frappe.call({				
-														method: 'moldex3d_integration.moldex3d_integration.doctype.mac_file.mac_file.update_moldex_mac',
-												
-														args: {
-															doctype:list_view.doctype,
-															docname: r.message,
-															data_file: data.message.file_url
-														}
-													})
-													.then(r => {
-														var options = [];
-												
-														list_view.refresh();
-													});
-											
-												}
-											})
 										}
 									}
 								});
@@ -96,6 +56,24 @@ frappe.listview_settings['MAC File'] = {
 		});
 		list_view.page.change_inner_button_type('Process Mac File',null, 'dark');
 
+	},
+
+	// upload_and_attach_macfile: function (r, macfiledata, list_view) {
+	upload_and_attach_macfile: function (doctype, docname, macfiledata) {
+		let macfile = new FormData();
+
+		macfile.append('doctype',doctype);
+		macfile.append('docname', docname);
+		macfile.append('folder', "Home/"+doctype);
+		macfile.append('file', macfiledata);
+
+		fetch('/api/method/moldex3d_integration.moldex3d_integration.doctype.mac_file.mac_file.upload_attach_macfile', {
+			headers: {
+				'X-Frappe-CSRF-Token': frappe.csrf_token
+			},
+			method: 'POST',
+			body: macfile
+		})
 	},
 	
     
